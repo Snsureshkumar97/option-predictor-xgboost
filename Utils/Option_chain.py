e202113c2a80814d4b9a5e25fa9532dc65f8d38e
Snsureshkumar97 <snsureshkumar97@gmail.com>
2025-06-22 04:30:53 -0400

    Create Option_chain.py

---
+++ Utils/Option_chain.py
@@ -0,0 +1,7 @@
+def get_option_data():
+    # Simulated Zerodha option chain data response
+    return {
+        "strike": "49700 CE",
+        "premium": 122.0,
+        "features": [0.9, 1.2, 15000, 0.65, 0.8]  # Example model features
+    }
